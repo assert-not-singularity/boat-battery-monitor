603545d2e7d0d7b4f2949890bf24f95ca6840c5f
Niklas Puller <niklas.puller@outlook.com>
2020-04-29 21:24:45 +0200

    feat: Add program which prints the voltage to the console

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.Spi;
+using System.Threading;
+using Iot.Device.Adc;
+
+namespace BatMon
+{
+    class Program
+    {
+        const int R1 = 99_920;
+        const int R2 = 22_000;
+
+        static void Main(string[] args)
+        {
+            var gpio = new GpioController();
+
+            var spiSettings = new SpiConnectionSettings(0, 0)
+            {
+                ClockFrequency = 50_000
+            };
+
+            using (var spi = SpiDevice.Create(spiSettings))
+            {
+                var mcp = new Mcp3204(spi);
+
+                while (true)
+                {
+                    float value = mcp.Read(0) / 4096f * 3.318f;
+
+                    // Voltage divider
+                    float voltage = value * (R1 + R2) / R2;
+
+                    Console.WriteLine(voltage);
+
+                    Thread.Sleep(200);
+                }
+            }
+        }
+    }
+}
---
+++ install_service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp boat-battery-monitor.service /etc/systemd/system
+sudo systemctl enable boat-battery-monitor
