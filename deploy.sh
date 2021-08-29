87ad71ce9bd1b05920165d9604b07c3189bdfc4c
Niklas Puller <26602675+assert-not-singularity@users.noreply.github.com>
2021-08-29 13:16:39 +0200

    refactor: Change target platform to .NET 5.0

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-dotnet publish -r linux-arm -f netstandard2.0 -o bin/linux-arm/publish --self-contained false
-sshpass -p 'raspberry' rsync -auv bin/linux-arm/publish/* pi@raspberrypi.local:~/boat-battery-monitor/
+dotnet publish -r linux-arm -f net5.0 -o bin/linux-arm/publish --self-contained false
+sshpass -p 'bananapi' rsync -auv bin/linux-arm/publish/* pi@bananapi.local:~/boat-battery-monitor/
