a7d3c928d8ad83b8714f022f7dac969de8ff49d2
Niklas Puller <26602675+assert-not-singularity@users.noreply.github.com>
2022-04-17 16:58:21 +0200

    refactor: Change target platform to .NET 6.0

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-dotnet publish -r linux-arm -f net5.0 -o bin/linux-arm/publish --self-contained false
+dotnet publish -r linux-arm -f net6.0 -o bin/linux-arm/publish --self-contained false
 sshpass -p 'bananapi' rsync -auv bin/linux-arm/publish/* pi@bananapi.local:~/boat-battery-monitor/
